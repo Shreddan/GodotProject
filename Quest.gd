a6d271b623292f0a12df2ce1ad55aeb144c3e22f
Shreddan <shreddanity@gmail.com>
2023-10-07 14:32:50 +0100

    fixing stuff

---
+++ Quest.gd
@@ -0,0 +1,17 @@
+class_name Quest extends Node2D
+
+enum Diff {VERY_EASY, EASY, NORMAL, HARD, VERY_HARD}
+
+var Name = ""
+var Difficulty = Diff.VERY_EASY
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
